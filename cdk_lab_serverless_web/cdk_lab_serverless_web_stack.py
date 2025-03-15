a516a9917076b7f0f892f9faf9276ed4b33f0b90
kierawilson25 <60899868+kierawilson25@users.noreply.github.com>
2025-03-15 09:39:51 -0500

    initial commit

--- cdk_lab_serverless_web/cdk_lab_serverless_web_stack.py
+++ cdk_lab_serverless_web/cdk_lab_serverless_web_stack.py
@@ -1,19 +1,47 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     # aws_sqs as sqs,
 )
+
+from cdk_dynamo_table_view import TableViewer
 from constructs import Construct
+
+from .hitcounter import HitCounter
 
 class CdkLabServerlessWebStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        
+        #Create an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
+        
+        #instantiate the hit counter with my_lambda as the downstream function
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter', 
+            downstream=my_lambda
+        )
+        
+        #defines an API Gateway REST API resource backed by our "hello" function.
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
+        
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+        )
+        
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkLabServerlessWebQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
